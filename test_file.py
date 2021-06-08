26afda64e65f03947728aca60c2ccdd2b9373cfa
Despee <78901662+Despee@users.noreply.github.com>
2021-06-08 10:29:48 +0200

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -3,4 +3,4 @@
 // Copyright (c) 2009-2018 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-password:azertyuiop23091995
+password=azertyuiop23091995
